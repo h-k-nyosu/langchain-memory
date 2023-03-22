4146948edbed0c726ab1c858ba06514b73fabb3d
h-k-nyosu <kuro.h1020@gmail.com>
2023-03-23 07:43:58 +0900

    fix

--- app/api/routes/gpt4.py
+++ app/api/routes/gpt4.py
@@ -55,17 +55,22 @@ embeddings = OpenAIEmbeddings()
 
 # 非同期にURLを処理し、embeddingsとsplit_textsを生成するタスクを追加
 BackgroundTasks().add_task(process_urls_background, langchain_docs, callback=process_urls_and_generate_embeddings)
+docsearch = Chroma.from_texts(split_texts, embeddings)
+
 
 @router.post('/gpt4')
 async def gpt_endpoint(user_message: str):
-    docsearch = Chroma.from_texts(split_texts, embeddings)
     docs = docsearch.similarity_search(user_message, k=2)
     reference = [doc.page_content for doc in docs]
     print(f"reference: {reference}, len:{len(reference)}")
-    input = """ユーザーからの会話に答えてください。また会話に関係のあるかもしれない参考情報も渡します。必要に応じて参考情報を利用して回答してください。
-    ユーザーからの会話：{user_message}
+    input = """
+    {user_message}
 
-    参考情報：{reference}
+    なおあなたは外部のDBと接続されており、ユーザーから来た会話に最も関連する一部の情報を取得します。
+    {reference}
+    
+    ただし外部DBに存在するテキストは限られており、ユーザーの会話とは関係ない場合もあります。その場合は与えられた関連情報を無視してください。
+    回答の根拠が不確かな場合は「分からない」と答えてください。また回答する際は「但し書き」を絶対に含まないようにしてください。ユーザーの会話に対してストレートに答える必要があります。
     """.format(user_message=user_message, reference=reference)
     
     res = conversation.predict(input=input)
